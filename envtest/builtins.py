d63653f349992c2a3f7be8080d75386e20b7ae6f
acse-tnt24 <tyana.tshiota24@imperial.ac.uk>
2024-10-08 08:56:00 +0100

    add time script

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import datetime
 
-__all__ = ['rand_array','smooth_image', 'my_mat_solve']
+__all__ = ['rand_array','smooth_image', 'my_mat_solve', 'show_time']
 
 
 def rand_array(shape):
@@ -13,3 +14,7 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def show_time():
+    now = datetime.datetime.now()
+    return now
---
+++ scripts/tya_script.py
@@ -0,0 +1,5 @@
+from envtest import show_time
+
+current_time = show_time()
+
+print(current_time.strftime("%Y-%m-%d %H:%M:%S"))
